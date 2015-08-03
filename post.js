b41678d9d41449a41b0b73727483b1ed0d69cb99
Brian Johnson <johnba4@gmail.com>
2015-08-03 10:07:41 -0400

    added random user functionality and required the Giphy field

--- post.js
+++ post.js
@@ -8,6 +8,7 @@ var http = require('http');
 var request = require('request');
 
 app.use(urlencodedBodyParser);
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('view_engine','ejs');
@@ -36,26 +37,44 @@ app.get('/letters', function (req, res) {
 app.post('/letters', function (req, res) {
     var model = getModel();
     var id = makeNewId(model.letters);
-    var newLetter = {"id":id,"name":req.body.name,"addressOne":req.body.addressOne,"addressTwo":req.body.addressTwo,"city":req.body.city,"state":req.body.state,"zip":req.body.zip,"country":req.body.country,"text":req.body.text,"giphyWords":req.body.giphyWords};
     
-    newLetter.giphyWords = newLetter.giphyWords.match(/\w+/g);
-    
-    var JSONgiphyData = request("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+newLetter.giphyWords.join('+'), function (err, response, body) {
-        var giphyData = JSON.parse(body);
-        var giphyUrl = giphyData.data.image_url;
+    if (req.body.name === "" && req.body.addressOne === "" && req.body.addressTwo === "" && req.body.city === "" && req.body.state === "" && req.body.state === "") {
+        JSONrandomUser = request("https://randomuser.me/api/", function (err, response, body) {
+            var randomUserData = JSON.parse(body);
+            var randomUser = randomUserData.results[0].user;
+            var newLetter = {"id":id,"name":randomUser.name.first,"addressOne":randomUser.location.street,"addressTwo":"","city":randomUser.location.city,"state":randomUser.location.state,"zip":randomUser.location.zip,"country":randomUserData.nationality,"text":req.body.text,"giphyWords":req.body.giphyWords};
+            newLetter.giphyWords = newLetter.giphyWords.match(/\w+/g);
         
-        console.log(giphyData);
+            var JSONgiphyData = request("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+newLetter.giphyWords.join('+'), function (err, response, body) {
+                var giphyData = JSON.parse(body);
+                var giphyUrl = giphyData.data.image_url;
+
+                newLetter.giphyUrl = giphyUrl;
+                model.letters.push(newLetter);
+
+                var updatedData = JSON.stringify(model);
+                fs.writeFileSync('data.json', updatedData);
+                res.redirect('/letters');
+            });
+        })
+    } else {
+        var newLetter = {"id":id,"name":req.body.name,"addressOne":req.body.addressOne,"addressTwo":req.body.addressTwo,"city":req.body.city,"state":req.body.state,"zip":req.body.zip,"country":req.body.country,"text":req.body.text,"giphyWords":req.body.giphyWords};
+        
+        newLetter.giphyWords = newLetter.giphyWords.match(/\w+/g);
+        
+        var JSONgiphyData = request("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+newLetter.giphyWords.join('+'), function (err, response, body) {
+            var giphyData = JSON.parse(body);
+            var giphyUrl = giphyData.data.image_url;
+
+            newLetter.giphyUrl = giphyUrl;
+            model.letters.push(newLetter);
 
 
-
-        newLetter.giphyUrl = giphyUrl;
-        model.letters.push(newLetter);
-        
-        
-        var updatedData = JSON.stringify(model);
-        fs.writeFileSync('data.json', updatedData);
-        res.redirect('/letters');    
-    });    
+            var updatedData = JSON.stringify(model);
+            fs.writeFileSync('data.json', updatedData);
+            res.redirect('/letters');
+        });
+    }    
 });
 
 app.get('/letters/new', function (req, res) {
